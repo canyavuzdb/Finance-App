eda5894493de814006caed249d2751fd6edeed6b
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-03-25 23:19:20 +0300

    Update

--- FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinShark/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48705204819696fda0afb6a5b0d493b7ca67602")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b17b62867807fa0f86181a298a49f0bb3202ad7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
