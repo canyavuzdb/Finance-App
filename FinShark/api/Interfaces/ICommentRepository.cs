1fb9cbe80243c9012ef3cd40f7c3f58003586772
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-03-26 18:13:07 +0300

    Update ICommentRepository.cs

--- FinShark/api/Interfaces/ICommentRepository.cs
+++ FinShark/api/Interfaces/ICommentRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Comment;
 using api.Models;
 
 namespace api.Interfaces
@@ -11,7 +12,7 @@ namespace api.Interfaces
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment comment);
-        // Task<Comment?> UpdateAsync(int id, UpdateCommentRequestDto commentDto);
-        // Task<Comment?> DeleteAsync(int id);
+        Task<Comment?> UpdateAsync(int id, Comment commentModel);
+        Task<Comment?> DeleteAsync(int id);
     }
 }
