f629c4ed83d0f0dbcafe664c9c534d2d7676e12e
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-03-26 17:37:42 +0300

    CreateAsync

--- FinShark/api/Interfaces/ICommentRepository.cs
+++ FinShark/api/Interfaces/ICommentRepository.cs
@@ -10,7 +10,7 @@ namespace api.Interfaces
     {
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
-        // Task<Comment> CreateAsync(Comment comment);
+        Task<Comment> CreateAsync(Comment comment);
         // Task<Comment?> UpdateAsync(int id, UpdateCommentRequestDto commentDto);
         // Task<Comment?> DeleteAsync(int id);
     }
