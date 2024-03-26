970f20301d7a3db35d44ee7daded7bdb566b680d
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-03-26 16:52:24 +0300

    GetByIdAsync Added

--- FinShark/api/Interfaces/ICommentRepository.cs
+++ FinShark/api/Interfaces/ICommentRepository.cs
@@ -9,7 +9,7 @@ namespace api.Interfaces
     public interface ICommentRepository
     {
         Task<List<Comment>> GetAllAsync();
-        // Task<Comment?> GetByIdAsync(int id);
+        Task<Comment?> GetByIdAsync(int id);
         // Task<Comment> CreateAsync(Comment comment);
         // Task<Comment?> UpdateAsync(int id, UpdateCommentRequestDto commentDto);
         // Task<Comment?> DeleteAsync(int id);
