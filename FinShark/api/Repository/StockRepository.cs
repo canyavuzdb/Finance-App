42eae877d733e779cacb41a4418853d8cb514e40
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-04-13 22:07:24 +0300

    QueryObject feature

--- FinShark/api/Repository/StockRepository.cs
+++ FinShark/api/Repository/StockRepository.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
-using api.Models;   
+using api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Repository
@@ -13,8 +14,9 @@ namespace api.Repository
     public class StockRepository : IStockRepository
     {
 
-        private readonly ApplicationDBContext _context; 
-        public StockRepository(ApplicationDBContext context){
+        private readonly ApplicationDBContext _context;
+        public StockRepository(ApplicationDBContext context)
+        {
             _context = context;
         }
 
@@ -29,18 +31,28 @@ namespace api.Repository
         {
             var stock = await _context.Stocks.FirstOrDefaultAsync(x => x.ID == id);
 
-            if(stock == null)
+            if (stock == null)
             {
                 return null;
             }
             _context.Remove(stock);
             await _context.SaveChangesAsync();
-            return stock;   
+            return stock;
         }
 
-        public async Task<List<Stock>> GetAllAsync()
+        public async Task<List<Stock>> GetAllAsync(QueryObject queryObject)
         {
-            return await _context.Stocks.Include(x => x.Comments).ToListAsync();
+            var stocks = _context.Stocks.Include(x => x.Comments).AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(queryObject.Symbol))
+            {
+                stocks = stocks.Where(x => x.Symbol.Contains(queryObject.Symbol));
+            }
+            if (!string.IsNullOrWhiteSpace(queryObject.CompanyName))
+            {
+                stocks = stocks.Where(x => x.CompanyName.Contains(queryObject.CompanyName));
+            }
+            return await stocks.ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
@@ -56,7 +68,7 @@ namespace api.Repository
         public async Task<Stock?> UpdateAsync(int id, UpdateStockRequestDto stockDto)
         {
             var existingStock = _context.Stocks.FirstOrDefault(x => x.ID == id);
-            if(existingStock == null)
+            if (existingStock == null)
             {
                 return null;
             }
