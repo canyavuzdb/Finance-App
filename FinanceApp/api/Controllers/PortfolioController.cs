4f71d723d1a2db9672e3858b5414803d74749116
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-05-02 21:25:15 +0300

    cruD HttpDelete

--- FinanceApp/api/Controllers/PortfolioController.cs
+++ FinanceApp/api/Controllers/PortfolioController.cs
@@ -60,5 +60,25 @@ namespace api.Controllers
             else { return Created(); }
 
         }
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> DeletePortfolio(string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+            var filteredStock = userPortfolio.Where(s => s.Symbol.ToLower() == symbol.ToLower()).ToList();
+               if (filteredStock.Count() == 1)
+            {
+                await _portfolioRepository.DeleteAsync(appUser, symbol);
+            }
+            else
+            {
+                return BadRequest("Stock not in your portfolio");
+            }
+
+            return Ok();
+        }
     }
 }
