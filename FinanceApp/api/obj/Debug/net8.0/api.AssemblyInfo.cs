1b99b042e25f499cffc243ae22ee8e3356afb5c7
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-05-02 20:47:22 +0300

    Update

--- FinanceApp/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ FinanceApp/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe1460fc8db7c5a015191c1561de6e7254cd6a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e55e1c7d3b393666dd0b96000490f1828706c258")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
