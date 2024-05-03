ab146208732389a664cc53d9c1dee5584dc0b968
Can Yavuz <88374853+canyavuzdb@users.noreply.github.com>
2024-05-04 01:26:19 +0300

    CORS added

--- FinanceApp/api/Program.cs
+++ FinanceApp/api/Program.cs
@@ -108,6 +108,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(x => x
+     .AllowAnyMethod()
+     .AllowAnyHeader()
+     .AllowCredentials()
+      //.WithOrigins("https://localhost:44351))
+      .SetIsOriginAllowed(origin => true));
 
 app.UseAuthentication();
 app.UseAuthorization();
